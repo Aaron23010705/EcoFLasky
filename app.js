96c1f7a5dcdcd2364f3bdf9073191bd65eb1bca7
Aarón García <Aaron.edgardo.garcia@gmail.com>
2025-03-19 10:34:30 -0600

    Reseñas (sin probar insert, put y delete) terminado

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 import express from "express";
 import clientsRoutes from "./src/sources/clients.js"
+import RatingRoutes from "./src/sources/ratings.js"
 import ProductsRoutes from "./src/sources/products.js"
 
 const app = express();
 app.use (express.json());
 
 app.use("/api/clients", clientsRoutes)
+app.use("/api/ratings", RatingRoutes)
 app.use("/api/products", ProductsRoutes)
 export default app;    
