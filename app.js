9799df0c4f5bccd136d25360695907d51e1918dd
Your Name <you@example.com>
2025-03-19 12:07:12 -0600

    commit sandoval

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 import express from "express";
+import SupplierRoutes from "./src/sources/SupplierRoutes.js"
+import CategoryRoutes from "./src/sources/CategoryRoutes.js"
 
 
 const app = express();
 app.use (express.json());
+
+app.use("/api/supplier", SupplierRoutes)
+app.use("/api/category", CategoryRoutes)
+
 export default app;    
