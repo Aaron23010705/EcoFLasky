f546823ff977e574a2cb9a746d623defbb386771
SofVer10 <20230106@ricaldone.edu.sv>
2025-03-19 10:54:13 -0600

    prueba

--- app.js
+++ app.js
@@ -1,10 +1,14 @@
 import express from "express";
 import distributorRoute from "./src/sources/distributor.js"
+import movementsRoutes from "./src/sources/movemnts.js"
+import shoppingCartRoutes from "./src/sources/shoppingCart.js"
 
 
 const app = express();
 app.use (express.json());
 
 app.use("/api/distributors", distributorRoute);
+app.use("/api/movements", movementsRoutes);
+app.use("/api/shoppingCart", shoppingCartRoutes);
 
 export default app;    
