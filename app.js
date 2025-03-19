8775da025aadaefa0a4dd283b3e2adb4674d3aa0
Aarón García <Aaron.edgardo.garcia@gmail.com>
2025-03-19 09:54:05 -0600

    Productos (sin probar insert, put y delete) terminado

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 import express from "express";
 import clientsRoutes from "./src/sources/clients.js"
-
+import ProductsRoutes from "./src/sources/products.js"
 
 const app = express();
 app.use (express.json());
 
 app.use("/api/clients", clientsRoutes)
+app.use("/api/products", ProductsRoutes)
 export default app;    
