5e51892d0d88d1bb5d8c143fdef8ea525df25440
sandoval <20200300@ricaldone.edu.sv>
2025-03-19 19:52:55 -0600

    commit sandoval

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import SupplierRoutes from "./src/sources/SupplierRoutes.js"
 import CategoryRoutes from "./src/sources/CategoryRoutes.js"
+import desingRouter from "./src/sources/desingRouter.js"
 
 
 const app = express();
@@ -8,5 +9,6 @@ app.use (express.json());
 
 app.use("/api/supplier", SupplierRoutes)
 app.use("/api/category", CategoryRoutes)
+app.use("/api/desings", desingRouter)
 
 export default app;    
